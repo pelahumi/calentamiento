aec330d779e48a8f6b60cb56cc8b8adb71d6f936
pelahumi <91721764+pelahumi@users.noreply.github.com>
2021-10-29 08:49:40 +0200

    Add files via upload

---
+++ ejercicio3.py
@@ -0,0 +1,8 @@
+#Suma
+print(21+43)
+#Resta
+print(142-52)
+#Multiplicación
+print(10*342)
+#Potencia
+print(5**2)
---
+++ ejercicio5.py
@@ -0,0 +1,46 @@
+dinero  = 2000
+hambre = 18
+precio_helado = 100
+interes = precio_helado * 0.2 
+
+print("Compro un helado")
+
+dinero  = dinero - precio_helado
+hambre = hambre + 18
+
+if hambre < 85:
+    print("Tengo hambre")
+
+print("Compro un helado")
+
+interes = precio_helado * 0.2
+precio_helado = precio_helado + interes
+dinero = dinero - precio_helado 
+hambre = hambre + 18
+
+if hambre < 85:
+     print("Sigo teniendo hambre")
+
+print("Compro otro helado")
+
+interes = precio_helado * 0.2
+precio_helado = precio_helado + interes
+dinero = dinero - precio_helado
+hambre = hambre + 18
+
+if hambre < 85:
+    print("Aun tengo hambre")
+
+print("Compro otro helado")
+
+interes = precio_helado * 0.2
+precio_helado = precio_helado + interes
+dinero = dinero - precio_helado
+hambre = hambre + 18
+
+if hambre < 85:
+    print("Necesito más helado")
+else:
+    print("Estoy lleno")
+    print("Me quedan: " + str(dinero) + " euros")
+    print("Estoy al " + str(hambre) + " de mi capacidad")
