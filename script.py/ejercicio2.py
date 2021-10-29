16af8efa9dac47dc73897c0db95d0676c74ac26e
pelahumi <91721764+pelahumi@users.noreply.github.com>
2021-10-29 08:53:08 +0200

    Delete script.py directory

--- script.py/ejercicio2.py
+++
@@ -1,7 +0,0 @@
-# This is an example
-print("Hello world")
-
-GREEN = '\033[32m'
-RESET = '\033[39m'
-
-print(GREEN+"Hello world")
--- script.py/ejercicio4.py
+++
@@ -1,4 +0,0 @@
-altura = 200
-altura = altura + 50
-print(altura)
-#La sintaxis son integer (números enteros)
