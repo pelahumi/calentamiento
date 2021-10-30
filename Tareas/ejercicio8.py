e29106f703a35777925e1a40ae867ff236e43b1b
Pelayo <phuermij@myuax.com>
2021-10-30 13:19:05 +0200

    Solucion de problemas

--- Tareas/ejercicio8.py
+++ Tareas/ejercicio8.py
@@ -1,13 +1,24 @@
+bitcoin_amount = input("Introduzca sus bitcoins: ")
+bitcoin_euros_value = 40000
+
 def bitcoinToEuros(bitcoin_amount, bitcoin_euros_value):
-    if bitcoin_euros_value < 30000:
-        print("Alerta")
-    elif bitcoin_euros_value < 30000:
-        print("Todo correcto")
-    return
-
-bitcoin_amount = input("Introduce la cantidad de bitcoin: ")
-valor_bitcoin = 30000
-bitcoin_euros_value = bitcoin_amount * valor_bitcoin
-print("Tienes: " + bitcoin_euros_value + "euros")
+    euros_amount = float(bitcoin_amount) * bitcoin_euros_value
+    if euros_amount < 30000:
+        print("Alerta su valor de Bicoin es de: " + str(euros_amount) + "€")
+    else:
+        print("Su valor de Bitoin ha subido a: "+ str(euros_amount) + "€")
+   
+bitcoinToEuros(bitcoin_amount,bitcoin_euros_value)
 
 
+bitcoin_amount = input("Introduzca sus bitcoins: ")
+bitcoin_euros_value = 25000
+
+def bitcoinToEuros(bitcoin_amount, bitcoin_euros_value):
+    euros_amount = float(bitcoin_amount) * bitcoin_euros_value
+    if euros_amount < 30000:
+        print("Alerta su valor de Bicoin es de: " + str(euros_amount) + "€")
+    else:
+        print("Su valor de Bitoin ha subido a: "+ str(euros_amount) + "€")
+   
+bitcoinToEuros(bitcoin_amount,bitcoin_euros_value)
